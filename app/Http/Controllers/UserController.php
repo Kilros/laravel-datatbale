f38b7913216639fdd6c6ed6cd601c8ca86548293
ngminhkhoi2302 <ngminhkhoi2302@gmail.com>
2022-12-10 20:21:59 +0700

    fix button

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,18 +8,18 @@ use Yajra\DataTables\DataTables as DataTablesDataTables;
 
 class UserController extends Controller
 {
-   public function index()
-   {
-      return view('user');
-   }
-   public function getUsers(Request $request)
+    public function index()
+    {
+        return view('user');
+    }
+    public function getUsers(Request $request)
     {
         if ($request->ajax()) {
             $data = User::select('id', 'name', 'email')->get();
             return DataTablesDataTables::of($data)
                 ->addIndexColumn()
                 ->addColumn('action', function($row){
-                    $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
+                    $actionBtn = '<a href="'.route('user.edit',$row['id']).'" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
                     return $actionBtn;
                 })
                 ->rawColumns(['action'])
@@ -27,4 +27,9 @@ class UserController extends Controller
        
         }
     }
+    public function editUser($id)
+    {
+        return $id;
+    }
+
 }
